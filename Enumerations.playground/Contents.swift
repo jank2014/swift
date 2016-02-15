01b4d426bd8268e43313d4e6818940730d416bb7
Jankz <jankz@jankz.local>
2016-02-15 19:16:49 +0800

    枚举类型

--- Enumerations.playground/Contents.swift
+++ Enumerations.playground/Contents.swift
@@ -119,7 +119,56 @@ print(" 18329004144 74vD8jyr")
 
 
 
-
-
-
 /******************************* 递归枚举 ******************************************/
+
+print("递归枚举")
+
+/* 
+
+递归枚举(recursive enumeration)是一种枚举类型,它有一个或多个枚举成员使用该枚举类型的实例作为关联 值。
+使用递归枚举时,编译器会插入一个间接层。你可以在枚举成员前加上 indirect 来表示该成员可递归。
+
+*/
+
+enum ArithmeticExpression {
+    case Number(Int)
+    indirect case Addition(ArithmeticExpression, ArithmeticExpression)
+    indirect case Multiplication(ArithmeticExpression, ArithmeticExpression)
+}
+
+
+//你也可以在枚举类型开头加上 indirect 关键字来表明它的所有成员都是可递归的:")
+
+print("你也可以在枚举类型开头加上 indirect 关键字来表明它的所有成员都是可递归的:")
+indirect enum ArithmeticExpressionTwo {
+    case Number(Int)
+    case Addition(ArithmeticExpression, ArithmeticExpression)
+    case Multiplication(ArithmeticExpression, ArithmeticExpression)
+
+}
+
+//该函数如果遇到纯数字,就直接返回该数字的值。如果遇到的是加法或乘法运算,则分别计算左边表达式和右边表达式的值,然后相加或相乘。
+
+func evaluate(expression: ArithmeticExpression) -> Int {
+    switch expression {
+    case .Number(let value):
+        return value
+    case .Addition(let left, let right):
+        return evaluate(left) + evaluate(right)
+    case .Multiplication(let left, let right):
+        return evaluate(left) * evaluate(right)
+    }
+    
+}
+
+let five = ArithmeticExpression.Number(5)
+let four = ArithmeticExpression.Number(4)
+let sum = ArithmeticExpression.Addition(five, four)
+let product = ArithmeticExpression.Multiplication(sum, ArithmeticExpression.Number(2))
+print(evaluate(product)) //输出18
+
+
+
+
+
+
